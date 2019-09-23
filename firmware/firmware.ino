58209fbc00b43d07dd5a504cd2c6515344f8c188
Kevin Guo <kg344@cornell.edu>
2019-09-23 14:33:32 -0400

    added moving code

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -5,7 +5,10 @@
 #define STOP 93
 #define RIGHT 91
 
-int victor_pins[6] = [2,3,4,5,6,7];
+int victor_pins[SERVO_NUM] = {2,3,4,5,6,7};
+int potentiometer_pins[SERVO_NUM] = {A0,A1,A2,A3,A4,A5};
+
+int current_states[SERVO_NUM] = {0,0,0,0,0,0};
 
 Servo servos[SERVO_NUM];
 
@@ -16,5 +19,20 @@ void setup() {
 }
 
 void loop() {
+  
+}
 
+void move(int ind){
+  int input = analogRead(potentiometer_pins[ind]);
+  int state;
+  if(input < 1023 && current_states[ind] == LEFT){
+    state = LEFT;
+  }
+  else if(input > 0 && current_states[ind] == RIGHT){
+    state = RIGHT;
+  }
+  else if(input == 1023){ state = RIGHT; }
+  else{ state = LEFT; }
+  servos[ind].write(state);
+  current_states[ind] = state;
 }
