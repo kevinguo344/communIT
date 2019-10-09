191364e3fbb48557823aab910933742deed1f8ed
Kevin Guo <kg344@cornell.edu>
2019-10-09 14:25:12 -0400

    changed how templates are handled

--- raspberry_pi_server/serial_control.py
+++ raspberry_pi_server/serial_control.py
@@ -8,16 +8,14 @@ app = Flask(__name__)
 
 ser = serial.Serial('/dev/ttyACM0',9600)
 
-templateData = {
-	'config1_state': "False",
-	'config2_state': "False",
-	'config3_state': "False",
-	'current_state:': "None",
-	'current_config': "None"
-}
-
 current_config = 0
-
+templateData = {
+		'config1_state': "False",
+		'config2_state': "False",
+		'config3_state': "False",
+		'current_state:': "None",
+		'current_config': "None"
+	}
 @app.route("/")
 def index():
 	return render_template('index.html', **templateData)
@@ -27,29 +25,37 @@ def action(config):
 	# checks which state the thing is
 	if config == "1":
 		new_config = "1"
-		templateData.config1_state = "True"
-		templateData.config2_state = "False"
-		templateData.config3_state = "False"
+		config1_state = "True"
+		config2_state = "False"
+		config3_state = "False"
 	if config == "2":
 		new_config = "2"
-		templateData.config1_state = "False"
-		templateData.config2_state = "True"
-		templateData.config3_state = "False"
+		config1_state = "False"
+		config2_state = "True"
+		config3_state = "False"
 	if config == "3":
 		new_config = "3"
-		templateData.config1_state = "False"
-		templateData.config2_state = "False"
-		templateData.config3_state = "True"
+		config1_state = "False"
+		config2_state = "False"
+		config3_state = "True"
 
 	if new_config != current_config:
 		current_config = new_config
 		ser.write(current_config)
-		templateData.current_config = current_config
+		current_config = current_config
 		if ser.read() == 'Done':
 			actual_state = "Complete"
 		else:
 			actual_state = "Aborted"
-	templateData.current_state = actual_state
+	current_state = actual_state
+
+	templateData = {
+		'config1_state': config1_state,
+		'config2_state': config2_state,
+		'config3_state': config3_state,
+		'current_state:': current_state,
+		'current_config': current_config
+	}
 	return render_template('index.html', **templateData)
 
 if __name__ == "__main__":
