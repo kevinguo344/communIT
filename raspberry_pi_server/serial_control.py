256e6bf209fa1275a4db7b2ecf35175611066d83
Kevin Guo <kg344@cornell.edu>
2019-10-09 22:57:13 -0400

    changed ser.read, will come back lateer

--- raspberry_pi_server/serial_control.py
+++ raspberry_pi_server/serial_control.py
@@ -47,11 +47,13 @@ def action(config):
 	if(current_config != new_config):
 		ser.write(str.encode(new_config))
 		current_config = new_config
-		if ser.read() == 'Done':
-			actual_state = "Complete"
-		else:
-			actual_state = "Aborted"
-		current_state = actual_state		
+		
+		#if ser.read() == 'Done':
+		#	actual_state = "Complete"
+		#else:
+		#	actual_state = "Aborted"
+		#current_state = actual_state
+		current_state = current_config
 	templateData = {
 		'title': title,
 		'config1_state': config1_state,
