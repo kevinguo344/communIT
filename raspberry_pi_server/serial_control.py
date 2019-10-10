c004524cc06e29b03858b083804d3311dc796924
Kevin Guo <kg344@cornell.edu>
2019-10-09 22:49:04 -0400

    added titles

--- raspberry_pi_server/serial_control.py
+++ raspberry_pi_server/serial_control.py
@@ -10,6 +10,7 @@ ser = serial.Serial('/dev/ttyACM0',9600)
 
 current_config = 0
 templateData = {
+		'title': "communIT Control Panel",
 		'config1_state': "False",
 		'config2_state': "False",
 		'config3_state': "False",
@@ -28,16 +29,19 @@ def action(config):
 		config1_state = "True"
 		config2_state = "False"
 		config3_state = "False"
+		title = "Config 1 Chosen"
 	if config == "2":
 		new_config = "2"
 		config1_state = "False"
 		config2_state = "True"
 		config3_state = "False"
+		title = "Config 2 Chosen"
 	if config == "3":
 		new_config = "3"
 		config1_state = "False"
 		config2_state = "False"
 		config3_state = "True"
+		title = "Config 3 Chosen"
 	global current_config
 	current_state = 0
 	if(current_config != new_config):
@@ -49,6 +53,7 @@ def action(config):
 			actual_state = "Aborted"
 		current_state = actual_state		
 	templateData = {
+		'title': title,
 		'config1_state': config1_state,
 		'config2_state': config2_state,
 		'config3_state': config3_state,
